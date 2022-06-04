b843d418713e1716c8e1a69724b2d3eb1f18d976
wang <xiaoqiaotq@gmail.com>
2022-06-04 13:23:44 +0800

    plugin update

--- www/printer.js
+++ www/printer.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0,arg1, success, error) {
+exports.print = function (arg0,arg1, success, error) {
     exec(success, error, 'PrintPlugin', 'print', [arg0,arg1]);
 };
